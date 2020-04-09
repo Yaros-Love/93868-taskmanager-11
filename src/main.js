5e740f94993ac15f193e7152201aaf6feff65c98
Nastya <zelenenkaya@gmail.com>
2020-04-09 21:27:07 +0300

    Created mock onjects and data structure

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 
-
-import { createSiteMenuTemplate } from "./components/site-menu.js";
-import { createFilterTemplate } from "./components/filter.js";
 import { createBoardTemplate } from "./components/board.js";
-import { createTaskTemplate } from "./components/task.js";
-import { createTaskEditTemplate } from "./components/task-edit.js";
+import { createFilterTemplate } from "./components/filter.js";
 import { createLoadMoreButtonTemplate } from "./components/load-more-button.js";
+import { createTaskEditTemplate } from "./components/task-edit.js";
+import { createSiteMenuTemplate } from "./components/site-menu.js";
+import { createTaskTemplate } from "./components/task.js";
 import { generateFilters } from "./mock/filter.js";
 import { generateTasks } from "./mock/task.js"
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -30,8 +31,22 @@ const boardElement = siteMainElement.querySelector(`.board`);
 
 render(taskListElement, createTaskEditTemplate(tasks[0]));
 
-for (let i = 1; i < tasks.length; i++) {
-  render(taskListElement, createTaskTemplate(tasks[i]));
-}
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+
+tasks.slice(1, showingTasksCount)
+  .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
 
 render(boardElement, createLoadMoreButtonTemplate());
+
+const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+  tasks.slice(prevTasksCount, showingTasksCount)
+    .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+  if (showingTasksCount >= tasks.length) {
+    loadMoreButton.remove();
+  }
+});
--- src/mock/task.js
+++ src/mock/task.js
@@ -1,5 +1,57 @@
+import { COLORS } from '../consts.js'
+
+const DefaultRepeatingDays = {
+  "mo": false,
+  "tu": false,
+  "we": false,
+  "th": false,
+  "fr": false,
+  "sa": false,
+  "su": false,
+};
+
+const DescriptionItems = [
+  `Изучить теорию`,
+  `Сделать домашку`,
+  `Пройти интенсив на соточку`,
+];
+
+const getRandomArrayItem = (array) => {
+  const randomIndex = getRandomIntegerNumber(0, array.length);
+
+  return array[randomIndex];
+};
+
+const getRandomIntegerNumber = (min, max) => {
+  return min + Math.floor(Math.random() * (max - min));
+};
+
+const getRandomDate = () => {
+  const targetDate = new Date();
+  const sign = Math.random() > 0.5 ? 1 : -1;
+  const diffValue = sign * getRandomIntegerNumber(0, 8);
+
+  targetDate.setDate(targetDate.getDate() + diffValue);
+
+  return targetDate;
+};
+
+const generateRepeatingDays = () => {
+  return Object.assign({}, DefaultRepeatingDays, {
+    "mo": Math.random() > 0.5,
+  });
+};
+
 const generateTask = () => {
-  return {};
+  const dueDate = Math.random() > 0.5 ? new Date() : getRandomDate();
+  return {
+    description: getRandomArrayItem(DescriptionItems),
+    color: getRandomArrayItem(COLORS),
+    dueDate,
+    repeatingDays: dueDate ? DefaultRepeatingDays : generateRepeatingDays(),
+    isArchive: Math.random() > 0.5,
+    isFavorite: Math.random() > 0.5,
+  };
 };
 
 const generateTasks = (count) => {
