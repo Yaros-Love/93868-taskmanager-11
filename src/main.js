83490fcb0adfed5bfb19f98cb6145accc2cc7319
Nastya <zelenenkaya@gmail.com>
2020-05-12 23:10:47 +0300

    add api.js file

--- src/main.js
+++ src/main.js
@@ -1,32 +1,15 @@
+import API from './api.js';
 import BoardComponent from "./components/board.js";
 import FilterController from "./controllers/filter.js";
 import SiteMenuComponent, {MenuItem} from "./components/site-menu.js";
 import StatisticsComponent from "./components/statistics";
-import {generateTasks} from "./mock/task.js";
+// import {generateTasks} from "./mock/task.js";
 import {render, RenderPosition} from "./utils/render.js";
 import BoardController from "./controllers/board.js";
 import TasksModel from "./models/tasks.js";
 
-const TASK_COUNT = 55;
-
-
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-
-const siteMenuComponent = new SiteMenuComponent();
-render(siteHeaderElement, siteMenuComponent);
-
-const tasks = generateTasks(TASK_COUNT);
-const tasksModel = new TasksModel();
-tasksModel.setTasks(tasks);
-
-const filterController = new FilterController(siteMainElement, tasksModel);
-filterController.render();
-const boardComponent = new BoardComponent();
-render(siteMainElement, boardComponent);
-
-const boardController = new BoardController(boardComponent, tasksModel);
-boardController.render();
+// const TASK_COUNT = 55;
+const AUTHORIZATION = `Basic dXNlckBwYXNzd29yZAo`;
 
 const dateTo = new Date();
 const dateFrom = (() => {
@@ -34,6 +17,24 @@ const dateFrom = (() => {
   d.setDate(d.getDate() - 7);
   return d;
 })();
+
+const api = new API(AUTHORIZATION);
+const tasksModel = new TasksModel();
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+const siteMenuComponent = new SiteMenuComponent();
+render(siteHeaderElement, siteMenuComponent);
+
+
+const filterController = new FilterController(siteMainElement, tasksModel);
+filterController.render();
+const boardComponent = new BoardComponent();
+render(siteMainElement, boardComponent);
+
+const boardController = new BoardController(boardComponent, tasksModel, api);
+
 const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
 render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
 statisticsComponent.hide();
@@ -56,3 +57,9 @@ siteMenuComponent.setOnChange((menuItem) => {
       break;
   }
 });
+
+api.getTasks()
+  .then((tasks) => {
+    tasksModel.setTasks(tasks);
+    boardController.render();
+  })
